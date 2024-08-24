96be946fffe8aa8e8239691c5a18f39067361719
Finradon <benedict.harder@pm.me>
2024-08-24 17:05:19 +0200

    quick change for hero selection

--- helden-tool.py
+++ helden-tool.py
@@ -9,84 +9,87 @@ with st.sidebar:
     nadoran = st.button("Nadoran", on_click=bt_funcs.change_hero, kwargs={"name": "Nadoran"})
     sdick = st.button("Sdick", on_click=bt_funcs.change_hero, kwargs={"name": "Sdick"})
 
+
 if 'hero' not in st.session_state:
-    with open('json/Sdick.json', 'r') as file:
-        data = file.read()
-    st.session_state['hero'] = hero.from_json(data)
-held: hero = st.session_state['hero']
-st.title(held.name)
+     st.title("Helden-Tool")
+     st.markdown("Ein Tool um Wunden, Niedrige LE und mehr für DSA4.1 zu verwalten")
+     st.markdown("Einfach links eine:n Held:in auswählen.")
+else:
+
+    held: hero = st.session_state['hero']
+    st.title(held.name)
 
 
-ratio = 100/held.max_lep
-st.progress(life_points_correction(int(held.lep*ratio)), f"**Lebenspunkte: {held.lep}/{held.max_lep}**")
+    ratio = 100/held.max_lep
+    st.progress(life_points_correction(int(held.lep*ratio)), f"**Lebenspunkte: {held.lep}/{held.max_lep}**")
 
-col1, col2 = st.columns([6, 1])
-with col1:
-    damage_slider = st.slider("**Schaden**", min_value=1, max_value=30)
-    #damage_input = st.number_input("**Schaden**", min_value=0)
+    col1, col2 = st.columns([6, 1])
+    with col1:
+        damage_slider = st.slider("**Schaden**", min_value=1, max_value=30)
+        #damage_input = st.number_input("**Schaden**", min_value=0)
 
-with col2:
-    tp_button1 = st.button("TP", on_click=held.receive_damage, kwargs={"value": damage_slider, "tp": True})
-    sp_button1 = st.button("SP", on_click=held.receive_damage, kwargs={"value": damage_slider, "tp": False})
-    #tp_button2 = st.button("Nr TP", on_click=held.receive_damage, kwargs={"value": damage_input, "tp": True})
-    #sp_button2 = st.button("Nr SP", on_click=held.receive_damage, kwargs={"value": damage_input, "tp": False})
-
-
-col11, col21 = st.columns([6, 1])
-with col11:
-    healing_slider = st.slider("**Heilung**", min_value=1, max_value=30)
-
-with col21:
-    st.markdown("######")
-    tp_button1 = st.button("Heilen", on_click=held.receive_healing, kwargs={"value": healing_slider })
+    with col2:
+        tp_button1 = st.button("TP", on_click=held.receive_damage, kwargs={"value": damage_slider, "tp": True})
+        sp_button1 = st.button("SP", on_click=held.receive_damage, kwargs={"value": damage_slider, "tp": False})
+        #tp_button2 = st.button("Nr TP", on_click=held.receive_damage, kwargs={"value": damage_input, "tp": True})
+        #sp_button2 = st.button("Nr SP", on_click=held.receive_damage, kwargs={"value": damage_input, "tp": False})
 
 
-c1, c2, c3, c4, c5 = st.columns(5)
-with c1:
-    st.metric("**Initiative**", held.ini)
-    st.metric("**LEP**", held.lep)
-with c2:
-    st.metric("**Rüstung**", held.rs)
-    st.metric("**LEP-Status**", held.state)
-with c3:
-    st.metric("**GS**", held.gs)
-    st.metric("**Wunden**", held.wound_count)
-with c4:
-    st.metric("**TP**", held.print_tp())
+    col11, col21 = st.columns([6, 1])
+    with col11:
+        healing_slider = st.slider("**Heilung**", min_value=1, max_value=30)
+
+    with col21:
+        st.markdown("######")
+        tp_button1 = st.button("Heilen", on_click=held.receive_healing, kwargs={"value": healing_slider })
 
 
-
-st.divider()
-
-col12, col22, col32 = st.columns(3)
-with col12:
-    # attack roll button
-    c1, c2 = st.columns([1, 3])
+    c1, c2, c3, c4, c5 = st.columns(5)
     with c1:
-        atbutton = st.button(f'AT{held.at}')
+        st.metric("**Initiative**", held.ini)
+        st.metric("**LEP**", held.lep)
     with c2:
-        if atbutton:
-            st.subheader(held.attack_roll())
-    ini = st.number_input(label="Ini", value=None, label_visibility="hidden", min_value=0)
-    ini_button = st.button("Set Ini", on_click=held.set_ini, kwargs={"value": ini})
-
-with col22:
-    # parry roll button
-    c1, c2 = st.columns([1, 3])
-    with c1:
-        pabutton = st.button(f'PA{held.pa}')
-    with c2:
-        if pabutton:
-            st.subheader(held.parry_roll())
-
-with col32:
-    # dodge roll button
-    c1, c2 = st.columns([1, 3])
-    with c1:
-        awbutton = st.button(f'AW{held.aw}')
-    with c2:
-        if awbutton:
-            st.subheader(held.dodge_roll())
+        st.metric("**Rüstung**", held.rs)
+        st.metric("**LEP-Status**", held.state)
+    with c3:
+        st.metric("**GS**", held.gs)
+        st.metric("**Wunden**", held.wound_count)
+    with c4:
+        st.metric("**TP**", held.print_tp())
 
 
-axx = st.toggle("**Axxeleratus**", on_change=held.toggle_axxeleratus)
+
+    st.divider()
+
+    col12, col22, col32 = st.columns(3)
+    with col12:
+        # attack roll button
+        c1, c2 = st.columns([1, 3])
+        with c1:
+            atbutton = st.button(f'AT{held.at}')
+        with c2:
+            if atbutton:
+                st.subheader(held.attack_roll())
+        ini = st.number_input(label="Ini", value=None, label_visibility="hidden", min_value=0)
+        ini_button = st.button("Set Ini", on_click=held.set_ini, kwargs={"value": ini})
+
+    with col22:
+        # parry roll button
+        c1, c2 = st.columns([1, 3])
+        with c1:
+            pabutton = st.button(f'PA{held.pa}')
+        with c2:
+            if pabutton:
+                st.subheader(held.parry_roll())
+
+    with col32:
+        # dodge roll button
+        c1, c2 = st.columns([1, 3])
+        with c1:
+            awbutton = st.button(f'AW{held.aw}')
+        with c2:
+            if awbutton:
+                st.subheader(held.dodge_roll())
+
+
+    axx = st.toggle("**Axxeleratus**", on_change=held.toggle_axxeleratus)
